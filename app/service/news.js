c65a2730e0cb9db00951bb11e1b1cf3402d6585a
Rumpel <hades137747575@gmail.com>
2017-10-20 20:00:00 +0800

    new service

--- app/service/news.js
+++ app/service/news.js
@@ -2,3 +2,28 @@
  * Created by Cai Kang Jie on 2017/12/8.
  */
 'use strict';
+
+const Service = require('egg').Service;
+
+class NewSerivce extends Service {
+  async list(page = 1) {
+    const { serviceUrl, pageSize } = this.config.news;
+    const { data: idList } = await this.ctx.curl(`${serviceUrl}/topstories.json`, {
+      data: {
+        orderBy: '"$key"',
+        startAt: `"${pageSize * (page - 1)}"`,
+        endAt: `"${pageSize * page - 1}"`,
+      },
+      dataType: 'json',
+    });
+    const newsList = await Promise.all(
+      Object.keys(idList).map(key => {
+        const url = `${serviceUrl}/item/${idList[key]}.json`;
+        return this.ctx.curl(url, { dataType: 'json' });
+      })
+    );
+    return newsList.map(res => res.data);
+  }
+}
+
+module.exports = NewSerivce;
